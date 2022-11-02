7c22d25cf7256e207a644a8e2707b2346146fe46
pflofif <bogdanvivchar8@gmail.com>
2022-11-02 19:44:23 +0200

    add step by step output

--- ASD10_Veronika/Form1.cs
+++ ASD10_Veronika/Form1.cs
@@ -4,7 +4,7 @@ namespace ASD10_Veronika
 {
     public partial class Form1 : Form
     {
-        private Lab10 _array;
+        private List<double> _array;
         public Form1()
         {
             InitializeComponent();
@@ -12,6 +12,7 @@ namespace ASD10_Veronika
 
         private void button2_Click(object sender, EventArgs e)
         {
+            richTextBox1.Clear();
             var correctSizeNumber =
                 Convert.ToInt32(
                     string.IsNullOrEmpty(textBox2.Text)
@@ -19,28 +20,32 @@ namespace ASD10_Veronika
                         : textBox2.Text
                 );
 
-            _array = new Lab10(correctSizeNumber);
+            _array = Enumerable.Range(0, Convert.ToInt32(correctSizeNumber))
+                .Select(n => (double)Random.Shared.Next(-50, 50))
+                .OrderBy(i => i)
+                .ToList();
+
             PrintArray();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            _array.DevideMax();
+            DevideMax();
             PrintArray();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            _array.SwapBehindMin();
+            SwapBehindMin();
             PrintArray();
         }
-        private void PrintArray(int? posOfFound = null)
+        public void PrintArray(int? posOfFound = null)
         {
-            richTextBox1.Clear();
+            //richTextBox1.Clear();
             for (int i = 0; i < _array.Count; i++)
             {
                 string stringToAdd = i == _array.Count - 1
-                        ? _array.Numbers.Last().ToString("F4")
+                        ? _array.Last().ToString("F4")
                         : $"{_array[i]} ,";
 
                 if (i == posOfFound)
@@ -62,7 +67,7 @@ namespace ASD10_Veronika
         private void button4_Click(object sender, EventArgs e)
         {
             int.TryParse(textBox1.Text, out var numberToFind);
-            int indexOfSearchable = _array.IndexOfSearcheble(numberToFind);
+            int indexOfSearchable = IndexOfSearcheble(numberToFind);
 
             if (indexOfSearchable == -1)
             {
@@ -71,6 +76,53 @@ namespace ASD10_Veronika
             }
 
             PrintArray(indexOfSearchable);
+        }
+        private void SwapBehindMin()
+        {
+            (_array[1], _array[2]) = (_array[2], _array[1]);
+        }
+        private void DevideMax()
+        {
+            double absSumOfNeg = Math.Abs(_array.TakeWhile(x => x < 0).Sum());
+            _array[^1] = _array.Last() / absSumOfNeg;
+        }
+
+        private int IndexOfSearcheble(int number)
+        {
+            richTextBox1.Clear();
+            var (min, max) = (0, _array.Count - 1);
+            var logAboutSearch = string.Empty;
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+
+                logAboutSearch += $"min: {min} , middle: {mid}, max: {max}\n";
+                richTextBox1.AppendText(logAboutSearch);
+
+                 Task.Delay(1000);
+
+                logAboutSearch = string.Empty;
+
+                if (number == _array[mid])
+                {
+                    return mid;
+                }
+
+                if (number < _array[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+
+                
+                //Thread.Sleep(2000);
+            }
+
+            return -1;
         }
     }
 }
